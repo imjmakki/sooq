c0a104b6331bb0bc61923c9568c588b966b0e909
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-26 18:10:21 +0300

    Sooq

---
+++ src/main/java/app/sooq/v1/SooqApplication.java
@@ -0,0 +1,13 @@
+package app.sooq.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SooqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SooqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/sooq/v1/SooqApplicationTests.java
@@ -0,0 +1,13 @@
+package app.sooq.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SooqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
